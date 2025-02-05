32ad04a524587ceaa2880729dc950ce02954ebef
Przemek <przemek@MacBook-Pro-Przemek.local>
2025-02-05 16:08:08 +0100

    update react

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
